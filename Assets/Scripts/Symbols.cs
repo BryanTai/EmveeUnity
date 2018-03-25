8237d1a0ce4a5b1ccd9c035a8ea329405ac63c6f
Bryan Tai <redhatpieman@gmail.com>
2018-03-25 10:34:31 -0700

    Added symbols

--- Assets/Scripts/Symbols.cs
+++ Assets/Scripts/Symbols.cs
@@ -1,3 +1,15 @@
-﻿public class Symbols {
-
+﻿public struct Symbols {
+    public int Mana;
+    public int Guardian;
+    //Spoil
+    public int Decay;
+    public int Growth;
+    //Spirits
+    public int Green;
+    public int Yellow;
+    public int Brown;
+    public int Wild;
+    //Victory Points
+    public int BlueVP;
+    public int GrayVP;
 }
